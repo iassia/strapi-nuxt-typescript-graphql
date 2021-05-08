bcfaac65ee2571d9475e5918ccf285f12993c43f
Fabio Iassia <fabio@instruct.com.br>
2021-05-08 20:18:30 -0300

    chore: remove cli auto-generated comments

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -2,7 +2,7 @@ export default {
   server: {
     host: '0.0.0.0',
   },
-  // Global page headers: https://go.nuxtjs.dev/config-head
+
   head: {
     title: 'frontend',
     htmlAttrs: {
@@ -18,21 +18,16 @@ export default {
     ]
   },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
   ],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
   ],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: ['@nuxt/typescript-build'],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
   modules: ['@nuxtjs/apollo'],
   apollo: {
     clientConfigs: {
@@ -42,7 +37,6 @@ export default {
     }
   },
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
   }
 }
